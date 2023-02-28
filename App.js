9b49f742092f4f11a54d98e49941dce7ba64bf14
luizatorrens <luiza.torrens.2006@gmail.com>
2023-02-28 16:51:22 -0300

    Projeto com header

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text>amo meu namorado</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
